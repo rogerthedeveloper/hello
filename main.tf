d5e3c1983d9629e91537e70149cb74bb4374ac88
Roger Sosa <rogerthedeveloper@gmail.com>
2024-09-04 22:00:00 -0600

    config

---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "us-west-2"  # Cambia a la región que prefieras
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.my_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.my_cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.my_cluster.token
+}
+
+data "aws_eks_cluster" "my_cluster" {
+  name = aws_eks_cluster.my_cluster.name
+}
+
+data "aws_eks_cluster_auth" "my_cluster" {
+  name = aws_eks_cluster.my_cluster.name
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = ["${aws_subnet.public_subnet_1.id}", "${aws_subnet.public_subnet_2.id}"]
+  }
+}
+
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = ["${aws_subnet.public_subnet_1.id}", "${aws_subnet.public_subnet_2.id}"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node
